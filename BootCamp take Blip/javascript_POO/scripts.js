511219b6e6e26a36c2d901febb8de144243954b0
rafaelbombonatto <rafael.bombonatto@gmail.com>
2022-02-07 14:11:12 -0300

    Aula OOJS

---
+++ BootCamp take Blip/javascript_POO/scripts.js
@@ -0,0 +1,76 @@
+class ContaBancaria
+{
+    constructor(agencia, numero, tipo)
+    {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    set saldo(valor)
+    {
+        this._saldo = valor;
+    }
+
+    sacar(valor){
+        if(valor > this._saldo)
+        {
+            return "Saldo Insuficiente"
+        }
+        this._saldo -= valor;
+        return this._saldo;
+    }
+
+    depositar(valor)    
+    {
+        this._saldo += valor;
+        return this._saldo;
+    }
+}
+
+class ContaCorrente extends ContaBancaria {
+    constructor(agencia, numero, cartoCredito){
+        super(agencia, numero);
+        this.tipo = 'corrente';
+        this._cartaoCredito = cartoCredito;
+    }
+
+    get cartaoCredito()
+    {
+        return this._cartaoCredito;
+    }
+
+    set cartaoCredito(valor)
+    {
+        this._cartaoCredito = valor;
+    }
+}
+
+class ContaPoupanca  extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero);
+        this.tipo = 'poupança';        
+    }
+}
+
+class ContaUniversitaria  extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero);
+        this.tipo = 'universitária';        
+    }
+
+    sacar(valor)
+    {
+        if (valor > 500) 
+        {
+             return "operação inválida apra o tipo de conta"
+        }
+
+        this._saldo -= valor;
+    }
+}
